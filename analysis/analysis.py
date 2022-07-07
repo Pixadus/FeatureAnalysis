96a8b0cf35af58a2e9122eecc2bf90d012104681
Parker Lamb <andromeda@outlook.ie>
2022-07-06 23:25:59 -0600

    Added analysis backend

---
+++ analysis/analysis.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue 7.5.22
+@title: Analysis
+@author: Parker Lamb
+@description: Script used to characterize OCCULT-2 identified features, including
+both breadth and length, as well as optionally supplied features.
+"""
+
+import numpy as np
+import cv2
+
+class Analysis:
+    def __init__(self, img_data, feature_data, axes):
+        """
+        Looks at characteristics of features. 
+
+        Parameters
+        ----------
+        img_data : ndarray
+            Data of the original image used for tracing
+        feature_data : OrderedDict
+            Format {f_num : [{'coord' : (x,y)}, {'coord' : (x,y)}, ... ]}
+        axes : pyplot.axes
+            Axes used to plot feature calculations on
+        """
+        super().__init__()
+
+        # Set variables
+        self.f_data = feature_data
+        self.img_data = img_data
+        self.ax = axes
+
+    def run(self):
+        """
+        Run the analysis.
+        """
+        # Take a look at all the custom options
+        self.analyze_cust()
+
+        # Get the breadth of the fibrils
+        self.get_breadth()
+    
+    def set_opts(self, opt_breadth=True, opt_length=True, opt_cust={}):
+        """
+        Set options for the analysis. External/internal.
+
+        Parameters
+        ----------
+        opt_breadth : bool
+            Enable per-coordinate breadth calculations.
+        opt_length : bool
+            Enable per-feature length calculations.
+        opt_cust : dict
+            Custom features, such as velocity, intensity, etc. of
+            format {param : image_path}. Image dimensions must match
+            those of the base image used for tracing.
+        """
+        self.opt_breadth = opt_breadth
+        self.opt_length = opt_length
+        self.opt_cust = opt_cust
+
+    def analyze_cust(self):
+        """
+        Analyze custom options.
+        """
+        for feature_num in self.f_data.keys():
+            for coord in self.f_data[feature_num]:
+                x = int(round(coord["coord"][0]))
+                y = int(round(coord["coord"][1]))
+                # Get values from custom options
+                for opt in self.opt_cust.keys():
+                    coord[opt] = self.opt_cust[opt][y,x]
+                # Remove any keys not in x,y or opt_cust
+                bad_keys = []
+                for key in coord.keys():
+                    if key not in ["coord"]+[k for k in self.opt_cust.keys()]:
+                        bad_keys.append(key)
+                for key in bad_keys:
+                    print(bad_keys)
+                    coord.pop(key)
+
+    def get_breadth(self):
+        """
+        Get the feature breadth on a per-coordinate basis.
+        """
+        def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
+            """Return a sharpened version of the image, using an unsharp mask."""
+            # From https://codingdeekshi.com/python-3-opencv-script-to-smoothen-or-sharpen-input-image-using-numpy-library/
+            blurred = cv2.GaussianBlur(image, kernel_size, sigma)
+            sharpened = float(amount + 1) * image - float(amount) * blurred
+            sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+            sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+            sharpened = sharpened.round().astype(np.uint8)
+            if threshold > 0:
+                low_contrast_mask = np.absolute(image - blurred) < threshold
+                np.copyto(sharpened, image, where=low_contrast_mask)
+            return sharpened
+        
+        # Convert to a format that CV2 can easily recognize
+        img_data = self.img_data*325
+        img_data = img_data.astype(np.uint8)
+        
+        # Create a sharpened image, then blur it a bit to get rid of noise
+        id_sharp = unsharp_mask(img_data, amount=10.0)
+        id_sharp_gauss = cv2.GaussianBlur(id_sharp, (5,5), 8.0)
+
+        # Get edges in the image
+        edges = cv2.Canny(id_sharp_gauss, threshold1=260, threshold2=280, apertureSize=7)
+
+        # Iterate over all features
+        for feature_num in self.f_data.keys():
+            # Counter to represent density of width segments
+            wctr = 0
+            # Get a list of all coordinates in feature
+            coords = [c['coord'] for c in self.f_data[feature_num]]
+            # TODO this is inefficient
+            for dict_coord, coord in zip(self.f_data[feature_num], coords):
+                # Increment the width counter
+                wctr +=1
+                # Get next and previous coordinates
+                nextcoord = next((i for i, val in enumerate(coords) if np.all(val == coord)), -1)+1
+                prevcoord = next((i for i, val in enumerate(coords) if np.all(val == coord)), -1)-1
+                if nextcoord > len(coords)-1:
+                    nextcoord = len(coords)-1
+                if prevcoord < 0:
+                    prevcoord = 0
+                nextcoord = coords[nextcoord]
+                prevcoord = coords[prevcoord]
+                # Calculate the slope at the coordinate
+                dx = nextcoord[1]-prevcoord[1]
+                dy = nextcoord[0]-prevcoord[0]
+                v = np.array([dx,dy])
+                # Get perpendicular vector
+                dp = np.empty_like(v)
+                dp[0] = -v[1]
+                dp[1] = v[0]
+                # Convert to unit vector
+                mag = np.sqrt(dp[0]**2+dp[1]**2)
+                dp[0] = dp[0]/mag
+                dp[1] = dp[1]/mag
+                # Array indices must be integers, rounding
+                dp[0] = round(dp[0])
+                dp[1] = round(dp[1])
+                # Variables to store x and y displacement vectors for width visualization
+                xs = []
+                ys = []
+                # Move in positive dp until we hit a Canny-identified edge
+                bp= 0
+                coord_offset = np.array([round(coord[1]),round(coord[0])])
+                while edges[coord_offset[0],coord_offset[1]] == 0:
+                    bp+=1
+                    xs.append(coord_offset[0])
+                    ys.append(coord_offset[1])
+                    coord_offset[0] = coord_offset[0]+dp[0]
+                    coord_offset[1] = coord_offset[1]+dp[1]
+                # Move in negative dp until we hit a Canny-identified edge
+                bn = 0
+                coord_offset = np.array([round(coord[1]),round(coord[0])])
+                while edges[coord_offset[0],coord_offset[1]] == 0:
+                    bn+=1
+                    xs.append(coord_offset[0])
+                    ys.append(coord_offset[1])
+                    coord_offset[0] = coord_offset[0]-dp[0]
+                    coord_offset[1] = coord_offset[1]-dp[1]
+                if (wctr % 5) == 0:
+                    self.ax.plot(ys,xs,markersize=1,linewidth=1, color='#a09516', alpha=0.7)
+                # Add width to coord characteristics
+                coord = np.append(coord,np.array([bp+bn]))
+                dict_coord['breadth'] = bp+bn
+                # TODO compare with previous coordinate width. If significantly larger, (i.e. 4 -> 12), set to previous
+                # coordinate width, as it's implied there is a error width here. 
--- analysis/widgets.py
+++ analysis/widgets.py
@@ -14,6 +14,8 @@ from PySide6.QtWidgets import (QCheckBox, QFileDialog, QFormLayout, QGroupBox, Q
 from matplotlib import (pyplot, colors)
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from astropy.io import fits
+from analysis.analysis import Analysis
+from collections import OrderedDict
 import csv
 
 class AnalysisWidget(QWidget):
@@ -46,6 +48,9 @@ class AnalysisWidget(QWidget):
         # Disable openDataButton until openImageButton
         self.openDataButton.setEnabled(False)
 
+        # Create the data variable
+        self.img_data = None
+
         # Add setup onclick events
         openImageButton.clicked.connect(self.open_image)
         self.openDataButton.clicked.connect(self.open_data)
@@ -57,18 +62,19 @@ class AnalysisWidget(QWidget):
         topBarLayout.addWidget(optionsBox)
 
         # Add check boxes to options
-        checkLength = QCheckBox("Calculate length")
-        checkBreadth = QCheckBox("Calculate breadth")
-        checkLength.setChecked(True)
-        checkBreadth.setChecked(True)
-        optionsLayout.addWidget(checkLength)
-        optionsLayout.addWidget(checkBreadth)
+        self.checkLength = QCheckBox("Calculate length")
+        self.checkBreadth = QCheckBox("Calculate breadth")
+        self.checkLength.setChecked(True)
+        self.checkBreadth.setChecked(True)
+        optionsLayout.addWidget(self.checkLength)
+        optionsLayout.addWidget(self.checkBreadth)
 
         # Add custom options box
         custBox = QGroupBox("Custom options")
         self.custLayout = QHBoxLayout()
         custBox.setLayout(self.custLayout)
         topBarLayout.addWidget(custBox)
+        self.custDict = {}
 
         # "Add" and "Remove" buttons
         addButton = QPushButton("+")
@@ -115,6 +121,9 @@ class AnalysisWidget(QWidget):
         self.analyzeButton.setMinimumWidth(175)
         resultLayout.addWidget(self.analyzeButton)
 
+        # Set the click functionality of the Analysis button
+        self.analyzeButton.clicked.connect(self.run_analysis)
+
         # Disable analyze button by default
         self.analyzeButton.setEnabled(False)
 
@@ -123,7 +132,6 @@ class AnalysisWidget(QWidget):
         resultsLayout = QFormLayout()
         resultsBox.setLayout(resultsLayout)
         resultLayout.addWidget(resultsBox)
-
 
     def open_image(self):
         """
@@ -137,8 +145,9 @@ class AnalysisWidget(QWidget):
         # Try to open data and set graph image
         try:
             f = fits.open(image_path, ignore_missing_end=True)
+            self.img_data = f[0].data
             self.ax.cla()
-            self.ax.imshow(f[0].data, origin="lower")
+            self.ax.imshow(self.img_data, origin="lower")
             # Refresh the canvas
             self.ax.draw_artist(self.ax.patch)
             self.canvas.update()
@@ -163,18 +172,26 @@ class AnalysisWidget(QWidget):
         # Try to open data and set graph data
         with open(data_path) as datafile:
             data = csv.reader(datafile)
-            fibril_num = 0
-            x = []
-            y = []
+            f_num = 0
+            self.f_data = OrderedDict()
+            # Initialize an empty coordinate list
+            self.f_data[f_num] = []
             for row in data:
-                if row[0] == fibril_num:
-                    x.append(float(row[1]))
-                    y.append(float(row[2]))
+                # If a coordinate in the same feature
+                if int(row[0]) == f_num:
+                    coord = {"coord" : (float(row[1]), float(row[2]))}
+                    self.f_data[f_num].append(coord)
+                # If a new feature
                 else:
-                    fibril_num = row[0]
-                    self.ax.plot(x,y, color="blue")
-                    x = [float(row[1])]
-                    y = [float(row[2])]
+                    x = [c["coord"][0] for c in self.f_data[f_num]]
+                    y = [c["coord"][1] for c in self.f_data[f_num]]
+                    self.ax.plot(x,y, color="blue", linewidth=1, markersize=1)
+                    # Set the new feature number
+                    f_num = int(row[0])
+                    coord = {"coord" : (float(row[1]), float(row[2]))}
+                    # Initialize the coordinate list, add current coord
+                    self.f_data[f_num] = [coord]
+
         # Refresh the canvas
         self.ax.draw_artist(self.ax.patch)
         self.canvas.update()
@@ -189,24 +206,94 @@ class AnalysisWidget(QWidget):
         """
         # Create a custom option container widget
         custOptWidget = QWidget()
+
         # Vertical layout
         custOptLayout = QVBoxLayout()
         custOptWidget.setLayout(custOptLayout)
+
         # Includes a line edit + a push button
         line = QLineEdit()
         line.setPlaceholderText("Label")
         custOptLayout.addWidget(line)
-        custOptLayout.addWidget(QPushButton("Add image"))
+        addImage = QPushButton("Add image")
+        custOptLayout.addWidget(addImage)
+
+        # Disable button by default
+        addImage.setEnabled(False)
+
+        # Add a event signal to button and text
+        line.textChanged.connect(lambda: self.modify_text(line, addImage))
+        addImage.clicked.connect(lambda: self.add_option_image(line))
+
         # Add to the custom options box layout
         self.custLayout.addWidget(custOptWidget)
+
         # Enable the "remButton"
         self.remButton.setEnabled(True)
+
+    def add_option_image(self, line_edit):
+        """
+        Add an image to the option.
+        """
+        dialog = QFileDialog()
+        # Only allow single, existing files
+        dialog.setFileMode(QFileDialog.ExistingFile)
+        # Image is a tuple of (path, file_type)
+        img_path = dialog.getOpenFileName(self, "Open option image", filter="FITS file (*.fits)")[0]
+        if len(img_path) == 0:
+            return
+        f = fits.open(img_path)
+        f = f[0].data
+        # Disable the line edit, so dict is correct
+        line_edit.setEnabled(False)
+        # Add the line text to the dict, associate it with the image data
+        self.custDict[line_edit.text()] = f
+    
+    def modify_text(self, line_edit, button):
+        """
+        Enable "Add image" button only if text in param field.
+        """
+        if line_edit.displayText() == '':
+            button.setEnabled(False)
+        else:
+            button.setEnabled(True)
 
     def rem_option(self):
         """
         Removes the latest option from the Custom Options box.
         """
+        # Remove the related entry from the custom opts dict
+        last_opt = self.custLayout.itemAt(self.custLayout.count()-1).widget()
+        text = None
+        for i in range(last_opt.layout().count()):
+            w = last_opt.layout().itemAt(i).widget()
+            try: 
+                text = w.displayText()
+                self.custDict.pop(text)
+            except:
+                pass
+                
         # Remove the last entry in the layout
         self.custLayout.itemAt(self.custLayout.count()-1).widget().setParent(None)
         if self.custLayout.count() == 0:
             self.remButton.setEnabled(False)
+
+    def run_analysis(self):
+        """
+        Runs the analysis on self.data
+        """
+        analysis = Analysis(self.img_data, self.f_data, self.ax)
+
+        analysis.set_opts(
+            self.checkBreadth.isChecked(), 
+            self.checkLength.isChecked(),
+            self.custDict
+            )
+
+        analysis.run()
+
+        # Refresh the canvas after the breadth updates
+        self.ax.draw_artist(self.ax.patch)
+        self.canvas.update()
+        self.canvas.flush_events()
+        self.canvas.draw()
